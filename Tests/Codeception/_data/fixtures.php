9737800ea53a035eaa73bc8579477fb582361f21
dmitrii <dmitrii.volkhin@oxid-esales.com>
2022-07-05 17:24:35 +0200

    Preparation for Codeception Test

---
+++ Tests/Codeception/_data/fixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+return [
+    // This product is available in ce|pe|ee demodata
+    'product' => [
+        'id' => 'dc5ffdf380e15674b56dd562a7cb6aec',
+        'title' => 'Kuyichi leather belt JEVER',
+        'bruttoprice_single' => '29.90',
+        'nettoprice_single' => '25.13',
+        'shipping_cost' => '3.90',
+        'currency' => '€'
+    ],
+
+];

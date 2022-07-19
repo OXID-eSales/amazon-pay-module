c68d5e50c91cf0956716f14e13b5688b457fb68e
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2022-07-19 18:12:35 +0300

    Set module settings in environment/1.yaml file and use them in tests

--- Tests/additional.inc.php
+++ Tests/additional.inc.php
@@ -3,20 +3,6 @@
 use OxidEsales\Eshop\Core\DatabaseProvider;
 use OxidEsales\Eshop\Core\Exception\DatabaseConnectionException;
 use OxidEsales\Eshop\Core\Exception\DatabaseErrorException;
-use OxidEsales\Facts\Facts;
-use Webmozart\PathUtil\Path;
-
-$facts    = new Facts();
-$settings = include Path::join(
-    __DIR__,
-    'Unit',
-    'amazonpayData.php'
-);
-
-define('STORE_ID', $settings['storeId']);
-define('MERCHANT_ID', $settings['merchantId']);
-define('PUBLIC_KEY_ID', $settings['publicKeyId']);
-define('PRIVATE_KEY', $settings['privateKey']);
 
 try {
     DatabaseProvider::getDb()->execute("DELETE FROM oxconfig WHERE  oxvarname = 'aModuleControllers'");

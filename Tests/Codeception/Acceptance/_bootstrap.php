fd15be399361a17b12e5dd9bc03929bae90ae272
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2022-07-22 19:20:08 +0300

    Return back BaseCest require

--- Tests/Codeception/Acceptance/_bootstrap.php
+++ Tests/Codeception/Acceptance/_bootstrap.php
@@ -10,6 +10,8 @@ declare(strict_types=1);
 // This is acceptance bootstrap
 use OxidEsales\Codeception\Module\FixturesHelper;
 
+require __DIR__ . '/BaseCest.php';
+
 $helper = new FixturesHelper();
 $helper->loadRuntimeFixtures(__DIR__ . '/../_data/fixtures.php');
 $helper->loadRuntimeFixtures(__DIR__ . '/../_data/amazonpayData.php');

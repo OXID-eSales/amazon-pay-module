e4aea8029c8a1405880a6ad987ed07e4fa53bdfa
dmitrii <dmitrii.volkhin@oxid-esales.com>
2022-07-28 15:51:42 +0200

    Update PSR-4

--- Tests/Codeception/Acceptance/_bootstrap.php
+++ Tests/Codeception/Acceptance/_bootstrap.php
@@ -10,8 +10,6 @@ declare(strict_types=1);
 // This is acceptance bootstrap
 use OxidEsales\Codeception\Module\FixturesHelper;
 
-require __DIR__ . '/BaseCest.php';
-
 $helper = new FixturesHelper();
 $helper->loadRuntimeFixtures(__DIR__ . '/../_data/fixtures.php');
 $helper->loadRuntimeFixtures(__DIR__ . '/../_data/amazonpayData.php');

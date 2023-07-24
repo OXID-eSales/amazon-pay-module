3a46047b37cb542968c992e4832ea3287b8b9bd7
smueller <stefan.mueller@fatchip.de>
2023-07-24 07:55:18 +0200

    added twig templates

---
+++ views/admin_twig/de/oscamazonpay_de_lang.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+$sLangName = 'Deutsch';
+$aLang = [
+    'charset'                          => 'UTF-8',
+    'amazonpay'                        => 'Amazon Pay',
+    'osc_amazonpay'                      => 'Amazon Pay Modul',
+    'OSC_AMAZONPAY_CONFIG'             => 'Konfiguration',
+    'OSC_AMAZONPAY_GENERAL'            => 'Allgemein',
+    'OSC_AMAZONPAY_CREDENTIALS'        => 'Anmeldeinformationen',
+    'OSC_AMAZONPAY_OPMODE'             => 'Betriebsmodus',
+    'OSC_AMAZONPAY_OPMODE_PROD'        => 'Produktion',
+    'OSC_AMAZONPAY_OPMODE_SANDBOX'     => 'Sandbox',
+    'HELP_OSC_AMAZONPAY_OPMODE'        => 'Verwenden Sie Sandbox (Test), um Amazon Pay zu konfigurieren und zu testen. Wenn Sie bereit sind,
+        echte Transaktionen zu empfangen, wechseln Sie zu Produktion (live).',
+    'OSC_AMAZONPAY_PRIVKEY'            => 'Privater Schlüssel',
+    'HELP_OSC_AMAZONPAY_PRIVKEY'       => 'Ihr privater Schlüssel für die Integration. Um ihn zu generieren, melden Sie sich bei Seller Central
+        an und gehen Sie dann zu Integration',
+    'OSC_AMAZONPAY_PUBKEYID'           => 'ID des öffentlichen Schlüssels',
+    'HELP_OSC_AMAZONPAY_PUBKEYID'      => 'Die ID des öffentlichen Schlüssels, die Sie zusammen mit dem in der Verkäuferzentrale generierten
+        privaten Schlüssel erhalten.',
+    'OSC_AMAZONPAY_MERCHANTID'         => 'Händler-ID',
+    'HELP_OSC_AMAZONPAY_MERCHANTID'    => 'Wenn Sie sich nicht sicher sind, wie Ihre Händler-ID lautet,
+         Melden Sie sich bei Seller Central an und gehen Sie dann zu Integration > MWS Access Key unter Allgemeine Informationen, Händler-ID.',
+    'OSC_AMAZONPAY_STOREID'            => 'Amazon-Client-ID',
+    'HELP_OSC_AMAZONPAY_STOREID'       => 'Melden Sie sich mit der Amazon-Client-ID an. Verwenden Sie nicht die Anwendungs-ID.
+        Rufen Sie diesen Wert ab von Login mit Amazon in Seller Central.',
+    'OSC_AMAZONPAY_PAYREGION'          => 'Zahlungsbereich',
+    'HELP_OSC_AMAZONPAY_PAYREGION'     => 'Die in Ihrem Shop erlaubten und bei Amazon Pay möglichen Währungen.',
+    'OSC_AMAZONPAY_DELREGION'          => 'Lieferbereich',
+    'HELP_OSC_AMAZONPAY_DELREGION'     => 'Die in Ihrem Shop erlaubten und bei Amazon Pay möglichen Lieferländer.',
+    'OSC_AMAZONPAY_SELLER'             => 'Verkäufer',
+    'OSC_AMAZONPAY_IPN'                => 'IPN-Endpunkt (bitte URL kopieren und im Amazon-Backend hinterlegen)',
+    'HELP_OSC_AMAZONPAY_IPN'           => 'IPN-Nachrichten werden von Amazon Pay ohne Ihr Zutun gesendet und können zur Aktualisierung Ihres
+        internen Auftragsverwaltungssystems und zur Bearbeitung von Bestellungen verwendet werden.<br>Besteht Ihre Server-/Shopkonfiguration aus mehreren URLs,
+        so tauschen Sie die vorgeschlagene Domain der o.g. URL gegen Ihre passende Domain aus und achten darauf, das die neue URL frei durch Amazon zugänglich ist.',
+    'OSC_AMAZONPAYEXPRESS_PLACEMENT'          => 'Platzierung Amazon Express',
+    'HELP_OSC_AMAZONPAYEXPRESS_PLACEMENT'     => 'Definieren Sie, wo die Amazon Pay-Schaltfläche in Ihrem Online-Shop angezeigt werden soll.',
+    'OSC_AMAZONPAY_PDP'                => 'Produktdetailseite',
+    'OSC_AMAZONPAY_MINICART_AND_MODAL' => 'Warenkorb + Warenkorb-PopUp',
+    'OSC_AMAZONPAY_PERFORMANCE'        => 'Performance',
+    'OSC_AMAZONPAY_EXCLUSION'          => '"AmazonPay ausschließen" nutzen',
+    'HELP_OSC_AMAZONPAY_EXCLUSION'     => 'Es können Produkte und Kategorien von AmazonPay ausgeschlossen werden. Wenn Sie das nicht tun, können Sie das Feature aus Performancegründen generell deaktivieren',
+    'OSC_AMAZONPAY_SOCIAL_LOGIN'        => 'Amazon Social Login',
+    'OSC_AMAZONPAY_SOCIAL_LOGIN_DEACTIVATED'        => 'Amazon Social Login deaktivieren',
+    'HELP_OSC_AMAZONPAY_SOCIAL_LOGIN_DEACTIVATED'   => 'Es besteht die Möglichkeit, sich im Shop mit einem Amazon-Kundenkonto anzumelden. Dabei werden die Adressdaten von Amazon übernommen. Die Anmeldung ist nur dann möglich, wenn noch kein Konto im Shop mit der gleichen Amazon-EMail-Adresse existiert.<br>Wenn Sie diese Funktion nicht Ihren Kunden anbieten wollen, können sie diese hier deaktivieren.',
+    'OSC_AMAZONPAY_SAVE'               => 'Speichern',
+    'OSC_AMAZONPAY_ERR_CONF_INVALID'   =>
+        'Ein oder mehrere Konfigurationswerte sind entweder nicht festgelegt oder falsch. Bitte überprüfen Sie sie noch einmal.<br>
+        <b>Modul inaktiv.</b>',
+    'OSC_AMAZONPAY_CONF_VALID'         => 'Konfigurationswerte OK.<br><b>Modul ist aktiv</b>',
+    'OSC_AMAZONPAY_CAPTYPE'            => 'Capture-Typ',
+    'HELP_OSC_AMAZONPAY_CAPTYPE'       => 'Einstufig erfasst die Zahlung sofort. Zweistufig erfasst die Zahlung nach dem Versand.',
+    'OSC_AMAZONPAY_CAPTYPE_ONE_STEP'   => 'Einstufig',
+    'OSC_AMAZONPAY_CAPTYPE_TWO_STEP'   => 'Zweistufig',
+    'OSC_AMAZONPAY_EXCLUDED'           => 'AmazonPay ausschließen',
+    'OSC_AMAZONPAY_CARRIER_CODE'       => 'Amazon Carrier Code',
+
+    'OSC_AMAZONPAY_PAYMENT_WAS_SHIPPING'    => 'Amazon-Zahlung nach Lieferung erfolgt',
+    'OSC_AMAZONPAY_PAYMENT_WHEN_SHIPPING'   => 'Amazon-Zahlung bei Lieferung erfolgt',
+    'OSC_AMAZONPAY_PAYMENT_DURING_CHECKOUT' => 'Amazon-Zahlung während des Checkouts erfolgt',
+    'OSC_AMAZONPAY_TRANSACTION_HISTORY'     => 'Transaktions-Historie',
+    'OSC_AMAZONPAY_IPN_HISTORY'             => 'IPN-Historie',
+    'OSC_AMAZONPAY_DATE'                    => 'Datum',
+    'OSC_AMAZONPAY_REFERENCE'               => 'Referenz',
+    'OSC_AMAZONPAY_RESULT'                  => 'Ergebnis',
+    'OSC_AMAZONPAY_REMARK'                  => 'Amazon Pay Mitteilung',
+    'GENERAL_ARTICLE_OSC_AMAZON_EXCLUDE'    => 'AmazonPay ausschließen',
+    'OSC_AMAZONPAY_REFUND'                  => 'Erstatten',
+    'OSC_AMAZONPAY_REFUND_ANNOTATION'       => 'Machen Sie eine Rückerstattung von bis zu ',
+    'OSC_AMAZONPAY_DELETE_ERROR'            => 'Bestellung kann nicht gelöscht werden, da bereits Geld zwischen dem Shop und Amazon transferiert wurde.',
+];
---
+++ views/admin_twig/en/oscamazonpay_de_lang.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+$sLangName = 'English';
+$aLang = [
+    'charset'                          => 'UTF-8',
+    'amazonpay'                        => 'Amazon Pay',
+    'osc_amazonpay'                      => 'Amazon Pay module',
+    'OSC_AMAZONPAY_CONFIG'             => 'Configuration',
+    'OSC_AMAZONPAY_GENERAL'            => 'General',
+    'OSC_AMAZONPAY_CREDENTIALS'        => 'Credentials',
+    'OSC_AMAZONPAY_OPMODE'             => 'Operation Mode',
+    'OSC_AMAZONPAY_OPMODE_PROD'        => 'Production',
+    'OSC_AMAZONPAY_OPMODE_SANDBOX'     => 'Sandbox',
+    'HELP_OSC_AMAZONPAY_OPMODE'        => 'To configure and test Amazon Pay, use Sandbox (test). When you\'re ready
+        to receive real transactions, switch to Production (live).',
+    'OSC_AMAZONPAY_PRIVKEY'            => 'Private Key',
+    'HELP_OSC_AMAZONPAY_PRIVKEY'       => 'Your private key for the integration. To generate it, sign in to Seller Central,
+        and then go to Integration',
+    'OSC_AMAZONPAY_PUBKEYID'           => 'Public Key ID',
+    'HELP_OSC_AMAZONPAY_PUBKEYID'      => 'The Public key ID you will recive together with the privatekey generated in the Seller Central.',
+    'OSC_AMAZONPAY_MERCHANTID'         => 'Merchant ID',
+    'HELP_OSC_AMAZONPAY_MERCHANTID'    => 'Amazon Pay merchant account identifier. If you are not sure what your merchant ID is,
+        sign in to Seller Central, and then go to Integration > MWS Access Key under General Information, Merchant ID.',
+    'OSC_AMAZONPAY_STOREID'            => 'Store ID',
+    'HELP_OSC_AMAZONPAY_STOREID'       => 'Login with Amazon client ID. Do not use the application ID. Retrieve this value
+        from Login with Amazon in Seller Central.',
+    'OSC_AMAZONPAY_PAYREGION'          => 'Payment Region',
+    'HELP_OSC_AMAZONPAY_PAYREGION'     => 'The currencies allowed in your shop and possible on Amazon Pay.',
+    'OSC_AMAZONPAY_DELREGION'          => 'Delivery Region',
+    'HELP_OSC_AMAZONPAY_DELREGION'     => 'The Deliverycountries allowed in your shop and possible on Amazon Pay.',
+    'OSC_AMAZONPAY_SELLER'             => 'Seller',
+    'OSC_AMAZONPAY_IPN'                => 'IPN Endpoint (please copy the URL and store it in the Amazon backend)',
+    'HELP_OSC_AMAZONPAY_IPN'           => 'IPN messages are sent by Amazon Pay without any action required on your part and
+         can be used to update your internal order management system and process orders.<br>If your server/shop configuration consists of several URLs,
+         so exchange the suggested domain of the above URL for your suitable domain and make sure that the new URL is freely accessible through Amazon.',
+    'OSC_AMAZONPAYEXPRESS_PLACEMENT'          => 'Placement of Amazon Express',
+    'HELP_OSC_AMAZONPAYEXPRESS_PLACEMENT'     => 'Define where to show the Amazon Express Pay button in your online store.',
+    'OSC_AMAZONPAY_PDP'                => 'Product Detail Page',
+    'OSC_AMAZONPAY_MINICART_AND_MODAL' => 'Basket + Basket-Modal',
+    'OSC_AMAZONPAY_PERFORMANCE'        => 'Performance',
+    'OSC_AMAZONPAY_EXCLUSION'          => 'use "Exclude AmazonPay"',
+    'HELP_OSC_AMAZONPAY_EXCLUSION'     => 'Products and categories can be excluded from AmazonPay. If you do not do this, you can generally deactivate the feature for performance reasons',
+    'OSC_AMAZONPAY_SOCIAL_LOGIN'        => 'Amazon Social Login',
+    'OSC_AMAZONPAY_SOCIAL_LOGIN_DEACTIVATED'        => 'deactivate Amazon Social Login',
+    'HELP_OSC_AMAZONPAY_SOCIAL_LOGIN_DEACTIVATED'   => 'It is possible to register in the shop with an Amazon customer account. The address data will be taken over from Amazon. Registration is only possible if there is no account in the shop with the same Amazon email address.<br>If you do not want to offer this function to your customers, you can deactivate it here.',
+    'OSC_AMAZONPAY_SAVE'               => 'Save',
+    'OSC_AMAZONPAY_ERR_CONF_INVALID'   =>
+        'One or more configuration values are either not set or incorrect. Please double check them.<br>
+        <b>Module inactive.</b>',
+    'OSC_AMAZONPAY_CONF_VALID'         => 'Configuration values OK.<br><b>Module is active</b>',
+    'OSC_AMAZONPAY_CAPTYPE'            => 'Capture Type',
+    'HELP_OSC_AMAZONPAY_CAPTYPE'       => 'One Step captures payment immediately. Two Step captures payment after shipping.',
+    'OSC_AMAZONPAY_CAPTYPE_ONE_STEP'   => 'One Step',
+    'OSC_AMAZONPAY_CAPTYPE_TWO_STEP'   => 'Two Step',
+    'OSC_AMAZONPAY_EXCLUDED'           => 'Exclude AmazonPay',
+    'OSC_AMAZONPAY_CARRIER_CODE'       => 'Amazon Carrier Code',
+
+    'OSC_AMAZONPAY_PAYMENT_WAS_SHIPPING'    => 'Amazon payment is made after shipping',
+    'OSC_AMAZONPAY_PAYMENT_WHEN_SHIPPING'   => 'Amazon payment is made on shipping',
+    'OSC_AMAZONPAY_PAYMENT_DURING_CHECKOUT' => 'Amazon payment is made during checkout',
+    'OSC_AMAZONPAY_TRANSACTION_HISTORY'     => 'Transaction-History',
+    'OSC_AMAZONPAY_IPN_HISTORY'             => 'IPN-History',
+    'OSC_AMAZONPAY_DATE'                    => 'Date',
+    'OSC_AMAZONPAY_REFERENCE'               => 'Reference',
+    'OSC_AMAZONPAY_RESULT'                  => 'Result',
+    'OSC_AMAZONPAY_REMARK'                  => 'Amazon Pay notice',
+    'GENERAL_ARTICLE_OSC_AMAZON_EXCLUDE'    => 'Exclude AmazonPay',
+    'OSC_AMAZONPAY_REFUND'                  => 'Refund',
+    'OSC_AMAZONPAY_REFUND_ANNOTATION'       => 'Make a refund of up to ',
+    'OSC_AMAZONPAY_DELETE_ERROR'            => 'Order cannot be deleted because money has already been transferred between the shop and Amazon.',
+];

c4291960ad7f3ee516232668fab5b8e2570875ad
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-01-25 15:46:42 +0100

    Take over from Branch 6.1

--- src/Core/AmazonService.php
+++ src/Core/AmazonService.php
@@ -656,7 +656,7 @@ class AmazonService
             ]
         );
 
-        $response = PhpHelper::jsonToArray($result['response']);
+        $response = $result['response'];
 
         if ($response['statusDetails']['state'] === 'Canceled' && $isCancelled === false) {
             $this->processCancel($orderId);

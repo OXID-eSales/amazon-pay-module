976fc46da4eae483e86c740d31fdf0b06532f74b
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-01-25 13:59:44 +0100

    The Result is everytime a array at this moment ...

--- src/Core/AmazonService.php
+++ src/Core/AmazonService.php
@@ -559,7 +559,7 @@ class AmazonService
             ]
         );
 
-        $response = is_array($result['response']) ? $result['response'] : PhpHelper::jsonToArray($result['response']);
+        $response = $result['response'];
 
         if ($result['status'] !== 200) {
             return;
@@ -656,7 +656,7 @@ class AmazonService
             ]
         );
 
-        $response = is_array($result['response']) ? $result['response'] : PhpHelper::jsonToArray($result['response']);
+        $response = $result['response'];
 
         if ($response['statusDetails']['state'] === 'Canceled' && $isCancelled === false) {
             $this->processCancel($orderId);

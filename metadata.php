7330fbb65629d3aacce4354e1a58dc1eaf4709aa
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-06-27 23:32:03 +0200

    revert Version Number

--- metadata.php
+++ metadata.php
@@ -60,7 +60,7 @@ $aModule = [
         'en' => 'Use of the online payment service from amazon.com'
     ],
     'thumbnail' => 'img/amazon-pay-logo.png',
-    'version' => '3.1.6-rc.2',
+    'version' => '3.1.6-rc.1',
     'author' => 'OXID eSales AG',
     'url' => 'https://www.oxid-esales.com',
     'email' => 'info@oxid-esales.com',

45afd4a26e29f131d373d1c3c0099fee5b7d6ac0
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-01-17 15:42:21 +0100

    PSAPC-261: casting string, added nullable typehint

--- src/Core/Logger.php
+++ src/Core/Logger.php
@@ -62,7 +62,7 @@ class Logger extends AbstractLogger
         $logMessage->setOrderId($context['orderId'] ?: $basket->getOrderId() ?: 'no basket');
         $logMessage->setShopId($context['shopId'] ?: Registry::getConfig()->getShopId());
         $logMessage->setRequestType($context['requestType'] ?? 'amazonpay');
-        $logMessage->setResponseMessage($message);
+        $logMessage->setResponseMessage((string)$message);
         $logMessage->setStatusCode($context['statusCode'] ?: '200');
         $logMessage->setIdentifier($context['identifier'] ?: $context['orderId'] ?: $userId);
         $logMessage->setChargeId($context['chargeId'] ?: 'null');

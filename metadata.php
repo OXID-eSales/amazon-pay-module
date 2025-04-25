3e266b0bf432eabc918739ab87dd2cfa0ee11562
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-04-25 12:48:06 +0200

    set AMAZON-EXPRESS as Payment in DetailsPage-Flow & new RC 1.6.0-rc.2

--- metadata.php
+++ metadata.php
@@ -59,7 +59,7 @@ $aModule = [
         'en' => 'Use of the online payment service from amazon.com'
     ],
     'thumbnail' => 'out/img/amazon-pay-logo.png',
-    'version' => '1.6.0-rc.1',
+    'version' => '1.6.0-rc.2',
     'author' => 'OXID eSales AG',
     'url' => 'https://www.oxid-esales.com',
     'email' => 'info@oxid-esales.com',

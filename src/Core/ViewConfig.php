fb732495ee025d1ab69b4a2d922f9332867c7c90
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-07-17 15:17:49 +0200

    remove unecessary isLastCheckoutStep

--- src/Core/ViewConfig.php
+++ src/Core/ViewConfig.php
@@ -396,15 +396,4 @@ class ViewConfig extends ViewConfig_parent
     {
         return Registry::getConfig();
     }
-
-    /**
-     * prevent showing the amazon button in last order step in apex theme
-     * @return bool
-     */
-    public function isLastCheckoutStep(): bool
-    {
-        $test = $this->getTopActionClassName();
-        $blIsLastStep = $test === 'order';
-        return $blIsLastStep;
-    }
 }

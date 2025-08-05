046dd8f20aae33ef2cee639298aa1592051d3b1f
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-08-05 13:26:27 +0200

    optimize comment

--- src/Core/AmazonService.php
+++ src/Core/AmazonService.php
@@ -912,7 +912,7 @@ class AmazonService
         $orderId = $repository->findOrderIdByChargeId($chargeId);
         $repository->markOrderPaid(
             $orderId,
-            'AmazonPay: ' . $amount,
+            'AmazonPay Captured: ' . $amount,
             'OK',
             $response['chargeId']
         );

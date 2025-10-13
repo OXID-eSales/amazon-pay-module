b7ef9e5ecda6b01277f6774e94c0d2038eee7b6d
Fatchip-Stefan <stefan.mueller@fatchip.de>
2025-10-13 17:36:52 +0200

    0126883: cleanup amazonpay express addresses before using normal amazonpay in the same session

--- src/Model/User.php
+++ src/Model/User.php
@@ -120,8 +120,10 @@ class User extends User_parent
     }
 
     public function changeUserData($sUser, $sPassword, $sPassword2, $aInvAddress, $aDelAddress) {
-        parent::changeUserData($sUser, $sPassword, $sPassword2, $aInvAddress, $aDelAddress);
         $session = Registry::getSession();
+        // remove possible previous amazonpay address from session
+        $session->setVariable(Constants::SESSION_DELIVERY_ADDR, '');
+        parent::changeUserData($sUser, $sPassword, $sPassword2, $aInvAddress, $aDelAddress);
         if ($session->getVariable('paymentid') === Constants::PAYMENT_ID) {
             Registry::getUtils()->redirect(
                 Registry::getConfig()->getShopHomeUrl() . 'cl=order',

3b6ef0d914ede19e2b9ca6e99a0dcd33f0c3f72d
Fatchip-Stefan <stefan.mueller@fatchip.de>
2026-04-21 06:44:52 +0200

    styling fixes

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -157,8 +157,11 @@ class OrderController extends OrderController_parent
         ) {
             $amazonConfig = oxNew(Config::class);
             if ($amazonConfig->getAmazonPayLogging()) {
-                $logger->log(LogLevel::DEBUG,
-                    \OxidEsales\Eshop\Core\Registry::getLang()->translateString('MESSAGE_PAYMENT_UNAVAILABLE_PAYMENT', 1) . PHP_EOL .
+                $logger->log(
+                    LogLevel::DEBUG,
+                    Registry::getLang()->translateString(
+                        'MESSAGE_PAYMENT_UNAVAILABLE_PAYMENT', 1
+                    ) . PHP_EOL .
                     'isAmazonpayment: ' . $isAmazonPayment . PHP_EOL .
                     'isAmazonSessionActive: ' . $isAmazonSessionActive . PHP_EOL .
                     'exclude: ' . $exclude . PHP_EOL
@@ -368,8 +371,11 @@ class OrderController extends OrderController_parent
 
         if ($amazonConfig->getAmazonPayLogging()) {
             $logger = new Logger();
-            $logger->log(LogLevel::ERROR,
-                \OxidEsales\Eshop\Core\Registry::getLang()->translateString('MESSAGE_PAYMENT_UNAVAILABLE_PAYMENT', 1) . PHP_EOL .
+            $logger->log(
+                LogLevel::ERROR,
+                Registry::getLang()->translateString(
+                    'MESSAGE_PAYMENT_UNAVAILABLE_PAYMENT', 1
+                ) . PHP_EOL .
                 'Response: ' . var_dump($result['response']) . PHP_EOL
             );
         }

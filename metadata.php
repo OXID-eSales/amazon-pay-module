facfb913bce4f463d4041b78cfb114f36bdfd1e9
Mario Lorenz <mario.lorenz@oxid-esales.com>
2026-09-04 16:51:09 +0200

    prepare v3.3.0-rc.3 + Refund-Mail-text more generic

--- metadata.php
+++ metadata.php
@@ -62,7 +62,7 @@ $aModule = [
         'en' => 'Use of the online payment service from amazon.com'
     ],
     'thumbnail' => 'img/amazon-pay-logo.png',
-    'version' => '3.3.0-rc.2',
+    'version' => '3.3.0-rc.3',
     'author' => 'OXID eSales AG',
     'url' => 'https://www.oxid-esales.com',
     'email' => 'info@oxid-esales.com',
